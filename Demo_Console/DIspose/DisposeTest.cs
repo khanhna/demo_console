9b045194d455dbf4e618739c9ce1a941afe957ad
khanh.nguyenan <vn00691699@gmail.com>
2022-12-02 20:34:18 +0700

    Add dispose test

---
+++ Demo_Console/DIspose/DisposeTest.cs
@@ -0,0 +1,71 @@
+﻿namespace Demo_Console.DIspose;
+
+public static class DisposeTest
+{
+    public static List<StreamReader> Placeholder = new();
+    public const string FilePath = "D:\\Workspace\\NetPractice\\Demo_Console\\test_file.docx";
+
+    public static void ReadLine()
+    {
+        var keepRunning = true;
+        while (keepRunning)
+        {
+            var input = Console.ReadLine();
+            if (input == "exit")
+            {
+                keepRunning = false;
+            }
+            
+            if(input == "test") TestFilePath();
+            
+            if(input == "1") ReadFileKeepReference();
+            if(input == "2") ReadFileWithoutKeepReference();
+        }
+        
+        Console.WriteLine("DisposeTest Finish!");
+    }
+
+    public static void ReadFileKeepReference()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            var file = new StreamReader(FilePath);
+            int counter = 0; 
+            
+            while (file.ReadLine() != null) {
+                counter++;  
+            }  
+            // file.Close();
+            Console.WriteLine($"{i} - File has {counter} lines.");
+            Placeholder.Add(file);
+        }
+        Console.WriteLine($"Placeholder have {Placeholder.Count} references!");
+    }
+    
+    public static void ReadFileWithoutKeepReference()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            var file = new StreamReader(FilePath);
+            int counter = 0;
+            
+            while (file.ReadLine() != null) {
+                counter++;  
+            }  
+            // file.Close();
+            Console.WriteLine($"{i} - File has {counter} lines.");
+        }
+    }
+
+    public static void TestFilePath()
+    {
+        using var file = new StreamReader(FilePath);
+        int counter = 0;
+            
+        while (file.ReadLine() != null) {
+            counter++;  
+        }  
+        file.Close();
+        Console.WriteLine($"File has {counter} lines.");
+    }
+}
--- Demo_Console/Program.cs
+++ Demo_Console/Program.cs
@@ -1,9 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Demo_Console.DIspose;
 using Demo_Console.ElasticSearch;
 
 Console.WriteLine("Hello, World!");
 
-var esClient = IndexBuilder.GetElasticClient( new Uri("https://localhost:9200"));
-var workspaceService = new WorkspaceIndexService(esClient);
-await workspaceService.ExecutePath();
+// var esClient = IndexBuilder.GetElasticClient( new Uri("https://localhost:9200"));
+// var workspaceService = new WorkspaceIndexService(esClient);
+// await workspaceService.ExecutePath();
+
+DisposeTest.ReadLine();
