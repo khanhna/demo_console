a4204cd3927157daa45df5cbbea7f9d241f4d161
khanh.nguyenan <vn00691699@gmail.com>
2023-03-28 13:52:12 +0700

    Fix some name and add an extension

---
+++ Demo_Console/Extensions/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Demo_Console.Extensions;
+
+public static class StringExtensions
+{
+    public static string CreateMD5(this string input)
+    {
+        // Use input string to calculate MD5 hash
+        using var md5 = MD5.Create();
+        var inputBytes = Encoding.ASCII.GetBytes(input);
+        var hashBytes = md5.ComputeHash(inputBytes);
+
+        // Convert the byte array to hexadecimal string
+        var sb = new StringBuilder();
+        foreach (var t in hashBytes)
+        {
+            sb.Append(t.ToString("X2"));
+        }
+        return sb.ToString();
+    }
+}
